effbc0de28ebde6c6b847af6a373df03ab74d918
ei-sugimoto <143775446+ei-sugimoto@users.noreply.github.com>
2024-05-06 21:30:22 +0900

    fontのフラグを追加 (#6)
    * fontのフラグを追加
    
    * lint修正

--- cmd/create.go
+++ cmd/create.go
@@ -2,10 +2,15 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"regexp"
 
 	"github.com/common-nighthawk/go-figure"
 	"github.com/spf13/cobra"
 )
+
+var font string
 
 var AsciingCmd = &cobra.Command{
 	Use:   "create",
@@ -18,11 +23,44 @@ var AsciingCmd = &cobra.Command{
 			outPutString += args[i]
 			outPutString += " "
 		}
-		myFigure := figure.NewFigure(outPutString, "", true)
+		var err error
+
+		if font, err = cmd.Flags().GetString("font"); err != nil {
+			fmt.Println("Error: ", err)
+		}
+
+		myFigure := figure.NewFigure(outPutString, font, true)
 		myFigure.Print()
 	},
 }
 
 func init() {
+	AsciingCmd.PersistentFlags().StringVar(&font, "font", "", "font name")
+	err := AsciingCmd.MarkFlagRequired("font")
+	if err != nil {
+		if font != "" {
+			AsciingCmd.Run = func(cmd *cobra.Command, args []string) {
+				err := VaridateFont(font) // ここでフォントの存在を確認します
+				if err != nil {
+					os.Exit(1)
+				}
+				figure.NewFigure(args[0], font, true).Print()
+			}
+		}
+	}
+
 	rootCmd.AddCommand(AsciingCmd)
+}
+
+func VaridateFont(font string) error {
+	if !regexp.MustCompile(`^[a-zA-Z0-9_-]+$`).MatchString(font) {
+		return fmt.Errorf("Invalid font name")
+	}
+	fontpath := "fonts/" + font + ".flf"
+
+	if _, err := os.Stat(fontpath); os.IsNotExist(err) {
+		log.Println(err)                    // 詳細なエラーメッセージをログに記録します
+		return fmt.Errorf("Font not found") // ユーザーには一般的なエラーメッセージを表示します
+	}
+	return nil
 }
